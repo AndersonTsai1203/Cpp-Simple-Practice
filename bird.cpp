f7deb5ba4c6996cacb0b78d05e1fef4150123ffc
AndersonTsai1203 <tsai.anderson1203@gmail.com>
2023-08-04 13:23:26 +0800

    updated file

--- bird.cpp
+++ bird.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-int main() {
-    cout << "  ___\n";
-    cout << " ('v')\n";
-    cout << "((___))\n";
-    cout << " ^   ^\n";
+int main()
+{
+    vector<string> msg {"  ___", " ('v')", "((___))", " ^   ^"};
+
+    for (const string& word : msg)
+    {
+        cout << word << "\n";
+    }
+    cout << endl;
+
     return 0;
 }
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int main()
+{
     cout << "Hello World, it is good to C you!\n";
+    
     cout << "I'm learning C++!\n";
+
     return 0;
 }
